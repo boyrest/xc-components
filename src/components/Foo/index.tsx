357e9c3262f8e2cdf50ab770d634ea8098b21a63
za-zhulousheng <zhulousheng@zhongan.io>
2022-04-23 22:24:34 +0800

    feat: update

--- src/components/Foo/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Form } from 'antd';
-import './index.less';
-
-export default () => {
-  return <div>123123</div>;
-};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-export { default as Foo } from './components/Foo';
 export { default as AntdFormRender } from './components/AntdFormRender';
